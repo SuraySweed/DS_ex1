b2d792d18be018ed6a4c133c0e77e2cdb42dea04
Suray Sweed <suray.sweed@campus.technion.ac.il>
2022-04-23 01:42:13 +0300

    companies and employees files with the system manager h file

---
+++ DS_ex1/employees_id.cpp
@@ -0,0 +1,38 @@
+#include "employees_id.h"
+
+EmployeeIdData::EmployeeIdData(int employeeID, int employerID, int salary, int grade) : 
+	employee_id(employeeID), employer_id(employerID), salary(salary), grade(grade) {}
+
+EmployeeIdData::EmployeeIdData(const EmployeeIdData& employeeIdData) :
+	employee_id(employeeIdData.employee_id), employer_id(employeeIdData.employer_id),
+	salary(employeeIdData.salary), grade(employeeIdData.grade) {}
+
+EmployeeIdData::~EmployeeIdData() {}
+
+EmployeeIdData& EmployeeIdData::operator=(const EmployeeIdData& emploee_id_data)
+{
+	if (this == &emploee_id_data) {
+		return *this;
+	}
+	employee_id = emploee_id_data.employee_id;
+	employer_id = emploee_id_data.employer_id;
+	salary = emploee_id_data.salary;
+	grade = emploee_id_data.grade;
+
+	return *this;
+}
+
+bool EmployeeIdData::operator<(const EmployeeIdData& other) const
+{
+	return (this->employee_id < other.employee_id);
+}
+
+bool EmployeeIdData::operator>(const EmployeeIdData& other) const
+{
+	return (this->employee_id > other.employee_id);
+}
+
+bool EmployeeIdData::operator==(const EmployeeIdData& other) const
+{
+	return (this->employee_id == other.employee_id);
+}
--- DS_ex1/employees_salary.h
+++ DS_ex1/employees_salary.h
@@ -1,4 +1,22 @@
 #ifndef EMPLOYEES_SALARY_H_
 #define EMPLOYEES_SALARY_H_
 
+class EmployeeSalaryData {
+private:
+	int employee_id;
+	int employer_id;
+	int salary;
+	int grade;
+
+public:
+	EmployeeSalaryData(int employeeID, int employerID, int salary, int grade);
+	EmployeeSalaryData(const EmployeeSalaryData& EmployeeSalaryData);
+	~EmployeeSalaryData();
+	EmployeeSalaryData& operator=(const EmployeeSalaryData&);
+
+	bool operator<(const EmployeeSalaryData& other) const;
+	bool operator>(const EmployeeSalaryData& other) const;
+	bool operator==(const EmployeeSalaryData& other) const;
+};
+
 #endif //EMPLOYEES_SALARY_H_
